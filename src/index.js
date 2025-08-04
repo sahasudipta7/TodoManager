13ced031776437202172e1fd84cf089cfd935cf6
Sudipta Saha <148531317+sahasudipta7@users.noreply.github.com>
2025-08-04 12:48:05 +0530

    create utilities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({
-    
+
 })
 
-connectDB();
+connectDB().
+then(()=>{
+    app.on("error",(error)=>{
+        console.log("Following error occurred: ",error);
+        throw error;    
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is listening on port: ${process.env.PORT}`);        
+    })
+}).
+catch((error)=>{
+    console.log("MONGODB connection failed: ",error);
+    process.exit(1);//#    
+})
