e80b96ce1ee180e28c9d6ee55ddd76795cb9d229
Sudipta Saha <148531317+sahasudipta7@users.noreply.github.com>
2025-09-02 17:53:01 +0530

    fix mongoDBconnect error

--- api/index.js
+++ api/index.js
@@ -1,9 +1,27 @@
 import { app } from "../src/app.js";
+import dotenv from "dotenv";
 import serverless from "serverless-http";
 import connectDB from "../src/db/index.js";
 
 // Connect once before handling requests
-connectDB();
+dotenv.config({
+
+})
+connectDB().
+then(()=>{
+    app.on("error",(error)=>{
+        console.log("Following error occurred: ",error);
+        throw error;    
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is listening on port: ${process.env.PORT}`);        
+    })
+}).
+catch((error)=>{
+    console.log("MONGODB connection failed: ",error);
+    process.exit(1);//#    
+})
+
 export default serverless(app);//new change for vercel
 //export default app;// new changes for vercel 
 
